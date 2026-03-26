be66538f44ef631d48671ebea846fe43d2a27a1f
fj <fj@shangminet.net>
2026-03-26 19:54:49 +0800

    指令测试

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from  appium.webdriver.mobilecommand import MobileCommand
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
-YOUR_APP_BUNDLE_ID="dadong.smartlock.com"
+APP_BUNDLE_ID="dadong.smartlock.com"
 
 # 1. 配置真机和 IPA 信息（核心：Bundle ID 必须正确）
 options = XCUITestOptions()
@@ -20,16 +20,54 @@ options = XCUITestOptions()
 # options.set_capability('usePrebuiltWDA', True)          # 使用预编译的 WebDriverAgent
 # options.set_capability('skipDeviceInitialization', True)# 跳过设备初始化（加速）
 
-options.set_capability("platformName","iOS")
-options.set_capability("appium:automationName","XCUITest")
+# options.set_capability("platformName","iOS")
+# options.set_capability("appium:automationName","XCUITest")
 # 2. 连接 Appium 服务
-driver = webdriver.Remote('http://localhost:8100', options=options)
+# 手机运行的端口是8100 driver不要直连手机/localhost:8100，否则driver command将无法发送
+# driver = webdriver.Remote('http://localhost:8100', options=options)
+# driver应该连接Appium 服务，使用appium指令启用appium服务，appium运行在4723端口
+driver = webdriver.Remote('http://127.0.0.1:4723', options=options)
+driver.start_session(options)
 
 button_clicked=[]
+
+def command_test():
+    try:
+        # driver.activate_app(APP_BUNDLE_ID)
+        # driver.execute_script('mobile: activateApp', {'appId': APP_BUNDLE_ID})
+
+        print(f"driver.get_status: {driver.get_status()}")
+        print(f"driver.orientation: {driver.orientation}")
+        print(f"driver.query_app_state: {driver.query_app_state(APP_BUNDLE_ID)}")
+        print(f"driver.touch_id: {driver.touch_id(True)}")
+        driver.shake()
+        # print(f"driver.app_strings: {driver.app_strings()}")
+        # print(f"driver.get_log: {driver.get_log("client") }")
+
+        # driver.get_log("client")
+        # driver.back()
+        # driver.activate_app(APP_BUNDLE_ID)
+        # time.sleep(3)
+        print(f"driver.terminate_app: {driver.terminate_app(APP_BUNDLE_ID)}")
+        print(f"driver.is_app_installed: {driver.is_app_installed(APP_BUNDLE_ID)}")
+        print(f"driver.background_app: {driver.background_app(3)}")
+
+        # time.sleep(3)
+        # driver.activate_app(APP_BUNDLE_ID)
+        driver.remove_app(APP_BUNDLE_ID)
+        # driver.install_app("/Users/smwl/Desktop/Projects/AppAutoTest/workplace/Payload.ipa")
+        # driver.close()
+
+    except Exception as e:
+        print(f"\nException:\n{e}")
+
 
 def click_button():
     all_elements = driver.find_elements(AppiumBy.XPATH, "//*")
     for index, element in enumerate(all_elements, 1):
+        if element.text in ["Safari浏览器"]:
+            driver.activate_app(APP_BUNDLE_ID)
+            break
         # print(f"element:{element}")
         # print(f"element.id: {element.id} element.session_i: {element.session_id}")
         element_type = element.get_attribute("type")  # 元素类型（如 XCUIElementTypeButton）
@@ -43,20 +81,26 @@ def click_button():
             element.click()
             button_clicked.append(element.id)
             break
+    else:
+        print(f" all_elements:{all_elements}")
 
     # WebDriverWait(driver, 3).until(
     #     lambda d: d.execute_script("mobile: activeAppInfo")["bundleId"] != YOUR_APP_BUNDLE_ID
     # )
     time.sleep(3)
 
-    print(f"TOUCH_ID: {driver.execute(MobileCommand.TOUCH_ID)}")
+    # print(f"TOUCH_ID: {driver.execute(MobileCommand.TOUCH_ID)}")
 
 
     # 3. 如果跳转，立即激活原应用
     # print("检测到外部跳转，正在返回原应用...")
     # driver.execute_script("mobile: activateApp", {"bundleId": YOUR_APP_BUNDLE_ID})
+    if len(button_clicked) > 10:
+        return
     click_button()
-click_button()
+# click_button()
+command_test()
+
 sys.exit(0)
 try:
     print(f"get_status => {driver.get_status()}")
